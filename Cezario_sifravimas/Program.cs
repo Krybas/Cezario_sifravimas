c46fa77016e49e3205425df7784f1a00d7a63b74
Arnas Šilaikis <s047127@ad.viko.lt>
2024-02-21 13:32:15 +0200

    Finished

--- Cezario_sifravimas/Program.cs
+++ Cezario_sifravimas/Program.cs
@@ -10,7 +10,7 @@ public class Program
         Console.WriteLine("Įveskite poslinkį: ");
         int poslinkis = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Pasirinkite Cezario šivravimo būdo numerį: \n" + "1. Su masyvu \n" + "2. Su ascii");
+        Console.WriteLine("Pasirinkite Cezario šivravimo būdo numerį: \n" + "1. Su masyvu \n" + "2. Su ASCII");
         int pasirinkimas = Convert.ToInt32(Console.ReadLine());
 
         string uzsifruotasZodis = uzsifvravimas(zodis, poslinkis, pasirinkimas);
@@ -18,8 +18,8 @@ public class Program
         string issifruotasZodis = issivravimas(uzsifruotasZodis, poslinkis, pasirinkimas);
 
         Console.WriteLine("Žodis: " + zodis);
+        Console.WriteLine("Pasirinktas būdas: " + pasirinkimas);
         Console.WriteLine("Poslinkis: " + poslinkis);
-        Console.WriteLine("pasirinkimas: " + pasirinkimas);
         Console.WriteLine("Užšifruotas Žodis: " + uzsifruotasZodis);
         Console.WriteLine("Iššifruotas Žodis: " + issifruotasZodis);
     }
@@ -47,7 +47,7 @@ public class Program
             case 1:
                 return arrayCezaris(zodis, -poslinkis);
             case 2:
-                return asciiCezaris(zodis, -poslinkis);
+                return asciiDesifravimas(zodis, poslinkis);
             default:
                 Console.WriteLine("Nežinomas pasirkimas: " + pasirinkimas);
                 Environment.Exit(0);
@@ -60,26 +60,40 @@ public class Program
         string abecele = "aąbcčdeęėfghiįyjklmnoprsštuųūvzž" +
                          "AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ" +
                          "0123456789";
-        char[] uzsifruotas = zodis.ToCharArray();
-        for (int i = 0; i < uzsifruotas.Length; i++)
+        char[] zodisArray = zodis.ToCharArray();
+        for (int i = 0; i < zodisArray.Length; i++)
         {
-            int indeksas = abecele.IndexOf(uzsifruotas[i]);
+            int indeksas = abecele.IndexOf(zodisArray[i]);
             if (indeksas != -1)
             {
-                uzsifruotas[i] = abecele[(indeksas + poslinkis) % abecele.Length];
+                int newIndeksas = (indeksas + poslinkis) % abecele.Length;
+                if (newIndeksas < 0)
+                {
+                    newIndeksas += abecele.Length;
+                }
+                zodisArray[i] = abecele[newIndeksas];
             }
-                
         }
-        return new string(uzsifruotas);
+        return new string(zodisArray);
     }
-    static string asciiCezaris(string uzsifruotas, int poslinkis)
+    static string asciiCezaris(string zodis, int poslinkis)
     {
-        char[] zodzioRaides = uzsifruotas.ToCharArray();
-        for (int i = 0; i < zodzioRaides.Length; i++)
+        char[] zodisArray = zodis.ToCharArray();
+        string rezultatas = "";
+        for (int i = 0; i < zodisArray.Length; i++)
         {
-            int ascii = (int)zodzioRaides[i];
-            zodzioRaides[i] = (char)(ascii + poslinkis % 256);
+            int ASCII = (int)zodisArray[i];
+
+            if (ASCII >= 32 && ASCII <= 127)
+            {
+                ASCII = ((ASCII - 32 + poslinkis) % 95) + 32; 
+                rezultatas += (char)ASCII;
+            }
         }
-        return new string(zodzioRaides);
+        return rezultatas;
+    }
+    static string asciiDesifravimas (string zodis, int poslinkis)
+    {
+        return asciiCezaris(zodis, 95 - poslinkis % 95);
     }
 }
